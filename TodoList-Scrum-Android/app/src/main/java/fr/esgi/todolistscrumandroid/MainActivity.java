a660a8ad636365d6d1948cd148a1c5e22f7e8061
Jean-Christophe MELIKIAN <jean.melikian@gmail.com>
2017-03-01 10:35:48 +0100

    Fix add task ui

--- TodoList-Scrum-Android/app/src/main/java/fr/esgi/todolistscrumandroid/MainActivity.java
+++ TodoList-Scrum-Android/app/src/main/java/fr/esgi/todolistscrumandroid/MainActivity.java
@@ -14,7 +14,6 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 
-import fr.esgi.todolistscrumandroid.model.RealmController;
 import io.realm.Realm;
 
 public class MainActivity extends AppCompatActivity {
